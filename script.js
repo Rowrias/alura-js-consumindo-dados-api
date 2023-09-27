f39ceda5257fa2d50c56c270fe7a42fb7a676ddb
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-09-27 09:16:17 -0300

    3.1

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-/*
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse cep não existe!')
+        } else
+            console.log(r)
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento concluído!'));
 
---- Código da conversa versão síncrono ---
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-mandaMensagem();
-
-console.log("Tchau tchau!");
-
-*/
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-setTimeout(mandaMensagem, 5000);
-
-console.log("Tchau tchau!");
+console.log(consultaCEP);
